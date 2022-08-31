0f0e26f0e1a1b7e6ffa947a0285f052da6275927
RolandoRp <rolyrp@hotmail.com>
2022-08-31 16:45:03 -0500

    juego-heroes

--- PracticaJavascript/jsheroes/heroes.js
+++ PracticaJavascript/jsheroes/heroes.js
@@ -1,6 +1,12 @@
+function iniciarJuego() {
+    
+}
+
 function seleccionarHeroe() {
     alert("Seleccionaste tu Heroe")
 }
 
 let botonHeroe = document.getElementById("boton-heroes")
 botonHeroe.addEventListener("click", seleccionarHeroe)
+
+window.addEventListener("load", iniciarJuego)
