ab81951b1a42b13ef9e1f99e677832d3549f6da0
RolandoRp <rolyrp@hotmail.com>
2022-09-02 18:02:07 -0500

    Calculadora

--- PracticaJavascript/Calculadora/codigosuma.js
+++ PracticaJavascript/Calculadora/codigosuma.js
@@ -1,10 +1,13 @@
-function resultado(n1,n2) {
+function resultado(n1,n2, resultado) {
     n1 = document.getElementById("n1").value;  
     n2 = document.getElementById("n2").value;
     
-    if (suma = parseFloat(n1) + parseFloat(n2)) {
-        document.getElementById("resultado").innerHTML = suma
+    if (isNaN(n1) || isNaN(n2)) {
+        resultado = "Introduzca un numero"
+    } else if (suma = parseFloat(n1) + parseFloat(n2)) {
+        resultado = suma
     } else if (resta = parseFloat(n1) - parseFloat(n2)) {
-        document.getElementById("resultado").innerHTML = resta
-    }
+        resultado = resta
+    }           
+    document.getElementById("resultado").innerHTML = resultado
 }
