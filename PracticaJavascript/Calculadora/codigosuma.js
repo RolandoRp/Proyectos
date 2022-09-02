a8ed35a75b228de14d4f89998f2e925f30aee3ad
RolandoRp <rolyrp@hotmail.com>
2022-09-02 16:58:35 -0500

    Calculadora

--- PracticaJavascript/Calculadora/codigosuma.js
+++ PracticaJavascript/Calculadora/codigosuma.js
@@ -1,8 +1,8 @@
-function resultado() {
+function resultado(n1,n2) {
     n1 = document.getElementById("n1").value;  
     n2 = document.getElementById("n2").value;
     
-    if (resultado = parseFloat(n1) + parseFloat(n2)) {
-        document.getElementById("resultado").innerHTML = resultado
+    if (suma = parseFloat(n1) + parseFloat(n2)) {
+        document.getElementById("resultado").innerHTML = suma
     }
 }
