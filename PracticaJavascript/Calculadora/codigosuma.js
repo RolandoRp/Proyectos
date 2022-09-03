89cc9afe6a17849e320d6afe65a038b2dc191f32
RolandoRp <rolyrp@hotmail.com>
2022-09-02 19:05:43 -0500

    Calculadora

--- PracticaJavascript/Calculadora/codigosuma.js
+++ PracticaJavascript/Calculadora/codigosuma.js
@@ -1,13 +1,47 @@
-function resultado(n1,n2, resultado) {
+function resultadosuma(n1,n2, resultado) {
     n1 = document.getElementById("n1").value;  
     n2 = document.getElementById("n2").value;
     
     if (isNaN(n1) || isNaN(n2)) {
-        resultado = "Introduzca un numero"
-    } else if (suma = parseFloat(n1) + parseFloat(n2)) {
+    resultado = "Es necesarios introducir dos números válidos"
+    } else if (suma = parseFloat(n1) + parseFloat(n2)) { 
         resultado = suma
-    } else if (resta = parseFloat(n1) - parseFloat(n2)) {
+    }
+    document.getElementById("resultado").innerHTML = resultado
+}
+
+function resultadoresta(n1,n2, resultado) {
+    n1 = document.getElementById("n1").value;  
+    n2 = document.getElementById("n2").value;
+    
+    if (isNaN(n1) || isNaN(n2)) {
+    resultado = "Es necesarios introducir dos números válidos"
+    } else if (resta = parseFloat(n1) - parseFloat(n2)) { 
         resultado = resta
-    }           
+    }
+    document.getElementById("resultado").innerHTML = resultado
+}
+
+function resultadomultiplicacion(n1,n2, resultado) {
+    n1 = document.getElementById("n1").value;  
+    n2 = document.getElementById("n2").value;
+    
+    if (isNaN(n1) || isNaN(n2)) {
+    resultado = "Es necesarios introducir dos números válidos"
+    } else if (mult = parseFloat(n1) * parseFloat(n2)) { 
+        resultado = mult
+    }
+    document.getElementById("resultado").innerHTML = resultado
+}
+
+function resultadodivision(n1,n2, resultado) {
+    n1 = document.getElementById("n1").value;  
+    n2 = document.getElementById("n2").value;
+    
+    if (isNaN(n1) || isNaN(n2)) {
+    resultado = "Es necesarios introducir dos números válidos"
+    } else if (div = parseFloat(n1) / parseFloat(n2)) { 
+        resultado = div
+    }
     document.getElementById("resultado").innerHTML = resultado
 }
