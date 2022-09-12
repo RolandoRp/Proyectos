315a412f133265e4895c1f634bafa2f0c1f538ef
RolandoRp <rolyrp@hotmail.com>
2022-09-12 18:53:18 -0500

    fibonacci

--- PracticaJavascript/Fibonacci/cfibonacci.js
+++ PracticaJavascript/Fibonacci/cfibonacci.js
@@ -6,6 +6,14 @@
 //3+5 = 8
 //n2 pasa a ser n1, resultado pasa a ser n2
 
-let n1 = 0
-let n2 = 0
+let n1 = 1
+let n2 = 1
 let resultado = 0
+
+for(i = 0; i <15; i++) {
+    resultado = n1 + n2
+    n2 = n1
+    n2 = resultado
+    document.write(resultado + "<br/>")
+
+}
