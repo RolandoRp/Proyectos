75d48cf40ed220dffbd3dddd7ffdee791da153d0
RolandoRp <rolyrp@hotmail.com>
2022-09-14 16:28:01 -0500

    felicitacion

--- PracticaJavascript/felicitacion/cfelicitacion.js
+++ PracticaJavascript/felicitacion/cfelicitacion.js
@@ -1,4 +1,4 @@
 function mensaje() {
     let nombre = document.getElementById("nomb").value
-    document.getElementById("felicitacion").innerHTML = ("Feliz dia del programador " + nombre)
+    document.getElementById("felicitacion").innerHTML = ("Feliz dia del programador " + nombre + " n.n")
 }
