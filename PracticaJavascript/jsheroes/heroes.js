2c7fd8d5e07a28f139db92ea0f95cabc449d7b41
RolandoRp <rolyrp@hotmail.com>
2022-09-23 17:08:56 -0500

    juego-heroes

--- PracticaJavascript/jsheroes/heroes.js
+++ PracticaJavascript/jsheroes/heroes.js
@@ -106,9 +106,9 @@ function combate() {
 
 function numeroVidas() {
     if (vidasEnemigo == 0) {
-        
+        mensajeFinal("La Vitoria es Tuya!!")
     } else if (vidasJugador == 0) {
-        
+        mensajeFinal("Has sido Derrotado")
     }
 }
 
