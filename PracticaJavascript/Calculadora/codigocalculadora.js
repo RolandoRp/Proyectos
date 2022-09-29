27acc727609ada78fc631ba216a1590e33827c61
RolandoRp <rolyrp@hotmail.com>
2022-09-29 17:12:21 -0500

    Calculadora

--- PracticaJavascript/Calculadora/codigocalculadora.js
+++ PracticaJavascript/Calculadora/codigocalculadora.js
@@ -29,25 +29,25 @@ function resultadoresta(n1,n2, resultado) {
     document.getElementById("resultado").innerHTML = resultado
 }
 
-function resultadomultiplicacion(n1,n2, resultado) {
-    n1 = document.getElementById("n1").value 
+function resultadomultiplicacion() {
+    n1 = document.getElementById("n1").value
     n2 = document.getElementById("n2").value
-    
+    mult = parseFloat(n1) * parseFloat(n2)
     if (isNaN(n1) || isNaN(n2)) {
-    resultado = "Es necesarios introducir dos números válidos"
-    } else if (mult = parseFloat(n1) * parseFloat(n2)) { 
+        resultado = "Es necesarios introducir dos números válidos"
+    } else { 
         resultado = mult
-    }
+    } 
     document.getElementById("resultado").innerHTML = resultado
 }
 
-function resultadodivision(n1,n2, resultado) {
+function resultadodivision() {
     n1 = document.getElementById("n1").value 
     n2 = document.getElementById("n2").value
-    
+    div = parseFloat(n1) / parseFloat(n2)
     if (isNaN(n1) || isNaN(n2)) {
-    resultado = "Es necesarios introducir dos números válidos"
-    } else if (div = parseFloat(n1) / parseFloat(n2)) { 
+        resultado = "Es necesarios introducir dos números válidos"
+    } else { 
         resultado = div
     }
     document.getElementById("resultado").innerHTML = resultado
