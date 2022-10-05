9ef19c73b3898a0d17954efbed62b62fefe81fec
RolandoRp <rolyrp@hotmail.com>
2022-10-05 16:54:41 -0500

    pagina-testimonios

--- PracticaReact/pagina-testimonios/src/componentes/Testimonio.js
+++ PracticaReact/pagina-testimonios/src/componentes/Testimonio.js
@@ -4,8 +4,9 @@ function Testimonio() {
   return (
     <div className="contenedor-testimonio">
     <img 
-      className="imagen-testimonio"
-      src="../imagenes/JeanneA.jpg"/>
+      className = "imagen-testimonio"
+      src = {require("../imagenes/JeanneA.jpg")}
+      alt = "Imagen Jalter"/>
     </div>
   );    
 }
