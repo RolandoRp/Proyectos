28919a09bc1ed96a2d9e904f4482e234237f2119
RolandoRp <rolyrp@hotmail.com>
2022-10-07 14:57:49 -0500

    pagina-testimonios

--- PracticaReact/pagina-testimonios/src/App.js
+++ PracticaReact/pagina-testimonios/src/App.js
@@ -4,6 +4,7 @@ import {Testimonio} from "./componentes/Servants.js";
 function App() {
   return (
     <div className="App">
+      <h1>Servants de Fate Grand Order</h1>
       <Testimonio />
     </div>
   );
