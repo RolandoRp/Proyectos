4f8e8539bc25912112257498f93add6cb694fdca
RolandoRp <rolyrp@hotmail.com>
2022-10-10 15:26:03 -0500

    pagina-testimonios

--- PracticaReact/pagina-testimonios/src/App.js
+++ PracticaReact/pagina-testimonios/src/App.js
@@ -6,7 +6,11 @@ function App() {
     <div className="App">
       <div className='contenedor-principal'>
         <h1>Servants de Fate Grand Order</h1>
-        <Testimonio />
+        <Testimonio 
+        nombre = ""
+        claseservant = ""
+        frase = ""
+        />
       </div>
     </div>
   );
