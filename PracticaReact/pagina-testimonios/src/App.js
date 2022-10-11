c5a870c5288c6575157c2cefa670c2291494dc6f
RolandoRp <rolyrp@hotmail.com>
2022-10-11 16:37:14 -0500

    pagina-testimonios

--- PracticaReact/pagina-testimonios/src/App.js
+++ PracticaReact/pagina-testimonios/src/App.js
@@ -5,19 +5,18 @@ function App() {
   return (
     <div className="App">
       <div className='contenedor-principal'>
-        <h1>Servants de Fate Grand Order</h1>
+        <h1>Fate Grand Order Servants</h1>
         <Servant 
         nombre = "Jeanne d'Arc (Alter)"
-        claseservant = "Clase Avenger"
-        frase = "If there is a God, surely I will be punished for this."
+        claseservant = "Class Avenger"
+        frase = "Servant, Avenger. Summoned upon your request. ...What's with that look? Come on, here's the contract."
         imagen = "JeanneA"
         />
 
         <Servant
         nombre = "Senji Muramasa"
-        claseservant = "Clase Saber"
-        frase = "I came with the mind of an elder at the brink of death, and a body that can give free rein to my desires immoderately.
-        This must be what they call feeling young again, right?"
+        claseservant = "Class Saber"
+        frase = "Saber, Senji Muramasa. I've answered to your summons. Although I'm only a blacksmith, as a Pseudo-Servant I should be capable enough by imitating samurai's. Hm? What's with that look? It's a face that says I-know-how-it-is while still being surprised...maybe I'm showing up just a bit too early...?"
         imagen = "Muramasa"
         />
       </div>
