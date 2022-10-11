179c6de02ff042b26b374b8fd002ecb0dd6b9b79
RolandoRp <rolyrp@hotmail.com>
2022-10-11 16:24:25 -0500

    pagina-testimonios

--- PracticaReact/pagina-testimonios/src/App.js
+++ PracticaReact/pagina-testimonios/src/App.js
@@ -12,6 +12,14 @@ function App() {
         frase = "If there is a God, surely I will be punished for this."
         imagen = "JeanneA"
         />
+
+        <Servant
+        nombre = "Senji Muramasa"
+        claseservant = "Clase Saber"
+        frase = "I came with the mind of an elder at the brink of death, and a body that can give free rein to my desires immoderately.
+        This must be what they call feeling young again, right?"
+        imagen = "Muramasa"
+        />
       </div>
     </div>
   );
