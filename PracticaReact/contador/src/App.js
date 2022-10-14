5f03d03a08048f92412032558bf1b231ede0fe92
RolandoRp <rolyrp@hotmail.com>
2022-10-14 15:08:47 -0500

    contador

--- PracticaReact/contador/src/App.js
+++ PracticaReact/contador/src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div className='logo-contenedor'>
+        <img 
+          className='logo'/>
+      </div>
     </div>
   );
 }
