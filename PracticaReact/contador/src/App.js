e6ae25eac24e36491b37b18bafa682e5e9f197cb
RolandoRp <rolyrp@hotmail.com>
2022-10-14 16:11:50 -0500

    contador

--- PracticaReact/contador/src/App.js
+++ PracticaReact/contador/src/App.js
@@ -4,12 +4,17 @@ import fimelogo from "./imagenes/fimelogo.png";
 function App() {
   return (
     <div className="App">
+      
       <div className='logo-contenedor'>
         <img 
           className='logo'
           src={fimelogo}
           alt="logo de fime"
         />
+
+      <div className='contenedor-contador'>
+        
+      </div>
       </div>
     </div>
   );
