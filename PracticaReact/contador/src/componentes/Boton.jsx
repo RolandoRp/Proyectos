2b6ad7972e0efd8b892bf5e403c67a0309d42732
RolandoRp <rolyrp@hotmail.com>
2022-10-19 17:29:01 -0500

    contador

--- PracticaReact/contador/src/componentes/Boton.jsx
+++ PracticaReact/contador/src/componentes/Boton.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 
-function boton({ texto, BotondeClic }) {
+function boton({ texto, BotondeClic, manejarClick }) {
     return (//Sintaxis de desestructuracion
-      <button className={ BotondeClic ? "boton-click" : "boton-reset" }> //operador ternario
+      <button className={ BotondeClic ? "boton-click" : "boton-reset" }
+      onClick={manejarClick}>
         {texto}
       </button>
     );
