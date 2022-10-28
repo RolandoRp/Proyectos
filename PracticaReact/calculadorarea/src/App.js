65cde5457a65a9bad292e7c9d1deee099bb25359
RolandoRp <rolyrp@hotmail.com>
2022-10-28 16:42:10 -0500

    calculadorare

--- PracticaReact/calculadorarea/src/App.js
+++ PracticaReact/calculadorarea/src/App.js
@@ -1,15 +1,16 @@
 
 import './App.css';
+import fimelogo from './Imagenes/fimelogo.png';
 
 function App() {
   return (
     <div className= 'App'>
       <div className='logo-contenedor'>
         <img 
-        src = {}
+        src = {fimelogo}
         className = 'logo'
         alt='logo' />
-        
+
 
       </div>
     </div>
