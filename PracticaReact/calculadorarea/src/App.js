ad05f76daa9d6589a696a43954210640b8d166bc
RolandoRp <rolyrp@hotmail.com>
2022-11-25 16:52:16 -0600

    calculadorare

--- PracticaReact/calculadorarea/src/App.js
+++ PracticaReact/calculadorarea/src/App.js
@@ -28,30 +28,30 @@ function App() {
 
         <div className='fila'>
           <Boton manejarClick = { agregarInput }>1</Boton>
-          <Boton>2</Boton>
-          <Boton>3</Boton>
-          <Boton>+</Boton>
+          <Boton manejarClick = { agregarInput }>2</Boton>
+          <Boton manejarClick = { agregarInput }>3</Boton>
+          <Boton manejarClick = { agregarInput }>+</Boton>
         </div>
         
         <div className='fila'>
-          <Boton>4</Boton>
-          <Boton>5</Boton>
-          <Boton>6</Boton>
-          <Boton>-</Boton>
+          <Boton manejarClick = { agregarInput }>4</Boton>
+          <Boton manejarClick = { agregarInput }>5</Boton>
+          <Boton manejarClick = { agregarInput }>6</Boton>
+          <Boton manejarClick = { agregarInput }>-</Boton>
         </div>
         
         <div className='fila'>
-          <Boton>7</Boton>
-          <Boton>8</Boton>
-          <Boton>9</Boton>
-          <Boton>*</Boton>
+          <Boton manejarClick = { agregarInput }>7</Boton>
+          <Boton manejarClick = { agregarInput }>8</Boton>
+          <Boton manejarClick = { agregarInput }>9</Boton>
+          <Boton manejarClick = { agregarInput }>*</Boton>
         </div>
         
         <div className='fila'>
-          <Boton>=</Boton>
-          <Boton>0</Boton>
-          <Boton>.</Boton>
-          <Boton>/</Boton>
+          <Boton manejarClick = { agregarInput }>=</Boton>
+          <Boton manejarClick = { agregarInput }>0</Boton>
+          <Boton manejarClick = { agregarInput }>.</Boton>
+          <Boton manejarClick = { agregarInput }>/</Boton>
         </div>
         
         <div className='fila'>
