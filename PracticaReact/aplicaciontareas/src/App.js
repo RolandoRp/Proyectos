f5eb1925669e276d65918ddf8f5bd794cc53de46
RolandoRp <rolyrp@hotmail.com>
2022-12-16 17:27:21 -0600

    aplicaciontareas

--- PracticaReact/aplicaciontareas/src/App.js
+++ PracticaReact/aplicaciontareas/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import fimelogo from './imagenes/fimelogo.png';
 
 function App() {
   return (
     <div className="app-tareas">
       <div className='fime-logo-contenedor'>
-        <img src={} className='fime-logo'/>
+        <img src={ fimelogo } className='fime-logo' />
 
       </div>
     </div>
