8e1eb7306ada3851cfeb32bc9dc135bb791d747b
RolandoRp <rolyrp@hotmail.com>
2022-12-16 17:10:20 -0600

    aplicaciontareas

--- PracticaReact/aplicaciontareas/src/App.js
+++ PracticaReact/aplicaciontareas/src/App.js
@@ -2,8 +2,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="app-tareas">
+      <div className='fime-logo-contenedor'>
+        <img src={} className='fime-logo'/>
+
+      </div>
     </div>
   );
 }
