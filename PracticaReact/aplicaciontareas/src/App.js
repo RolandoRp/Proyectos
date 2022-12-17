3e46177e70ca26fdce98cfcd0847181a1508d2ae
RolandoRp <rolyrp@hotmail.com>
2022-12-17 15:22:54 -0600

    aplicaciontareas

--- PracticaReact/aplicaciontareas/src/App.js
+++ PracticaReact/aplicaciontareas/src/App.js
@@ -6,7 +6,10 @@ function App() {
     <div className="app-tareas">
       <div className='fime-logo-contenedor'>
         <img src={ fimelogo } className='fime-logo' />
+      </div>
 
+      <div className='lista-principal-tareas'>
+        <h1>Mis Tareas</h1>
       </div>
     </div>
   );
