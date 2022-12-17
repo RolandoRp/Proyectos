8d0f69ea0ce299e63f57c24a259ce856c8d31320
RolandoRp <rolyrp@hotmail.com>
2022-12-17 16:09:42 -0600

    aplicaciontareas

--- PracticaReact/aplicaciontareas/src/componentes/tareas.jsx
+++ PracticaReact/aplicaciontareas/src/componentes/tareas.jsx
@@ -1,5 +1,19 @@
 import React from "react";
 
-function Tarea() {
-    
+function Tarea({ texto }) {
+    return (
+      <div className='tarea-contenedor'>
+        
+        <div className='tarea-texto'>
+          { texto }
+        </div>
+
+        <div className='icono-tarea'>
+          Eliminar
+        </div>
+
+      </div>
+    );
 }
+
+export default Tarea;
