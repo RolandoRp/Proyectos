62d21b44ca9af111bf11c1a0074e722c6ee9b41a
RolandoRp <rolyrp@hotmail.com>
2023-03-28 15:21:36 -0600

    aplicaciontareas

--- PracticaReact/aplicaciontareas/src/componentes/tareas.jsx
+++ PracticaReact/aplicaciontareas/src/componentes/tareas.jsx
@@ -2,15 +2,19 @@ import React from "react";
 import '../estilos/tarea.css'
 import { AiOutlineCloseCircle } from "react-icons/ai"
 
-function Tarea({ texto, completada }) {
+function Tarea({ id, texto, completada, completarTarea, eliminarTarea }) {
     return (
       <div className={completada ? 'tarea-contenedor completada' : 'tarea-contenedor'}>
         
-        <div className='tarea-texto'>
+        <div 
+        className='tarea-texto'
+        onClick={() => completarTarea(id)}>
           { texto }
         </div>
 
-        <div className='contenedor-icono-tarea'>
+        <div 
+        className='contenedor-icono-tarea'
+        onClick={() => eliminarTarea(id)}>
         <AiOutlineCloseCircle className='icono-tarea' />
         </div>
 
