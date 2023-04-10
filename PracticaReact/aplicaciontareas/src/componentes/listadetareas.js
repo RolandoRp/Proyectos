30f092402cf8ad8d9ab15e7da06ec14f17c46a86
RolandoRp <rolyrp@hotmail.com>
2023-04-10 13:37:12 -0600

    correcciones

--- PracticaReact/aplicaciontareas/src/componentes/listadetareas.js
+++ PracticaReact/aplicaciontareas/src/componentes/listadetareas.js
@@ -13,12 +13,12 @@ function Listadetareas () {
       const tareasActualizadas = [tarea, ...tareas];
       setTareas(tareasActualizadas);
     }
-  }
+  };
 
   const eliminarTarea = id => {
     const tareasActualizadas = tareas.filter(tarea => tarea.id !== id);
     setTareas(tareasActualizadas);
-  }
+  };
 
   const completarTarea = id => {
     const tareasActualizadas = tareas.map(tarea => {
