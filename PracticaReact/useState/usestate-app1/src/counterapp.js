798823a764becae1437dc799457f71a08b8f664c
RolandoRp <rolyrp@hotmail.com>
2023-04-14 13:45:20 -0600

    decrement

--- PracticaReact/useState/usestate-app1/src/counterapp.js
+++ PracticaReact/useState/usestate-app1/src/counterapp.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react'
 
 const CounterApp = () => {
-  const {counter, setCounter} = useState(0);
+  const [counter, setCounter] = useState(0);
   return (
     <div>
       <button onClick={() => setCounter(counter+1)}>
         Increment
+      </button>
+      <button onClick={() => setCounter(counter-1)}>
+        Decrement
       </button>
       <h1>Clicks: {counter}</h1>
     </div>
