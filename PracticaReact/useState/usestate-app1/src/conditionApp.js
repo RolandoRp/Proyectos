10befaa9e3e005e483f699cc5fad24aae2484f73
RolandoRp <rolyrp@hotmail.com>
2023-04-19 17:52:21 -0600

    imprimir booleano

--- PracticaReact/useState/usestate-app1/src/conditionApp.js
+++ PracticaReact/useState/usestate-app1/src/conditionApp.js
@@ -6,13 +6,17 @@ const ConditionApp = () => {
   return (
     <div>
       <button onClick={() => setCondition(!condition)}>
-        No presionar
+        El boton
       </button>
-      {
+      
+      {/*{
         condition
         ?<h1>No Presiones el boton</h1>
-        :<h1>G E Y</h1>
-      }
+        :<h1>Te dije que no presionaras el boton 7_7</h1>
+      }*/}
+      
+      <h1>Estado es: {condition.toString()}</h1>
+      
       <h1>ConditionApp</h1>
     </div>
   )
